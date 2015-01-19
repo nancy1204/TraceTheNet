c6e4ce63ba29dd234456a5e51609f2e6924be999
nancy1204 <nancyoliele@gmail.com>
2015-01-19 11:20:01 +0100

    ajout de je sais pas quoi

---
+++ TraceTheNet/src/ExecuteShellComand.java
@@ -0,0 +1,53 @@
+package arbre;
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+ 
+public class ExecuteShellComand {
+ 
+	public static void main(String[] args) {
+ 
+		ExecuteShellComand obj = new ExecuteShellComand();
+ 
+		String domainName = "google.com";
+ 
+		
+ 
+		//in windows
+		String []command ={"tracert yahoo.com"};
+		//exec("cmd /c echo %HOMEPATH% >d:\\.txt"); 
+		//String [] tableau={"ls",">","titi"};
+
+		String output = obj.executeCommand(command);
+ 
+		System.out.println(output);
+ 
+	}
+ 
+	private String executeCommand(String[] command) {
+ 
+		StringBuffer output = new StringBuffer();
+ 
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader = 
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+ 
+                        String line = "";			
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+ 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+ 
+		return output.toString();
+ 
+	}
+ 
+}
